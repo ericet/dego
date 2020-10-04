b5a6a3494b7ad3fc87c94bf4d5e9eb6d650895b5
ericet <ericetchen@gmail.com>
2020-10-04 14:53:00 -0400

    new dashboard

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-function getNftStats() {
+function getNftStats(timestamp) {
     return new Promise((resolve, reject) => {
-        const url = `https://api.blurt.buzz/dego_nft`;
+        const url = `https://api.blurt.buzz/dego_nft_issued/${timestamp}`;
         let stats = [];
         axios.get(url).then(function (response) {
             if (response.status == 200) {
@@ -12,33 +12,33 @@ function getNftStats() {
                 for (let d of data) {
                     let totalDegoAmount = 0;
                     let multiplier = 0;
-                    let image="";
+                    let image = "";
                     if (d.name === 'BRONZE') {
                         multiplier = 1;
-                        image="https://dego.finance/upload/small/1.png";
+                        image = "https://dego.finance/upload/small/1.png";
                     } else if (d.name === 'SILVER') {
                         multiplier = 2;
-                        image="https://dego.finance/upload/small/2.png";
+                        image = "https://dego.finance/upload/small/2.png";
 
                     } else if (d.name === 'GOLD') {
                         multiplier = 3;
-                        image="https://dego.finance/upload/small/3.png";
+                        image = "https://dego.finance/upload/small/3.png";
 
                     } else if (d.name === 'PLATINUM') {
                         multiplier = 4;
-                        image="https://dego.finance/upload/small/4.png";
+                        image = "https://dego.finance/upload/small/4.png";
 
                     } else if (d.name === 'DIAMOND') {
                         multiplier = 5;
-                        image="https://dego.finance/upload/small/5.png";
+                        image = "https://dego.finance/upload/small/5.png";
 
                     } else if (d.name === 'KRYPTONITE') {
                         multiplier = 100;
-                        image="https://dego.finance/upload/small/6.png";
+                        image = "https://dego.finance/upload/small/6.png";
 
                     }
                     totalDegoAmount += Number(d.count) * multiplier;
-                    stats.push({ name: d.name, count: d.count, percentage: (d.count / totalNFT * 100).toFixed(2), degoAmount:multiplier,value: totalDegoAmount,image:image })
+                    stats.push({ name: d.name, count: d.count, percentage: (d.count / totalNFT * 100).toFixed(2), degoAmount: multiplier, value: totalDegoAmount, image: image })
                 }
                 resolve(stats);
 
@@ -53,7 +53,7 @@ $(document).ready(async function () {
     const colors = ['#007ED6', '#52D726', '#FFEC00', '#FF7300', '#7CDDDD', '#FF0000'];
     const nftOptions = {
         cutoutPercentage: 0,
-        legend: { position: 'bottom', padding: 5, labels: { pointStyle: 'circle', usePointStyle: true } },
+        legend: { position: 'right', padding: 5, labels: { pointStyle: 'circle', usePointStyle: true } },
         plugins: {
             datalabels: {
                 formatter: (value, ctx) => {
@@ -67,38 +67,86 @@ $(document).ready(async function () {
                 },
                 color: '#fff',
             }
+        },
+        title: {
+            display: true,
+            text: 'Total Issued NFTs Distribution'
         }
+
+
     };
-    let htmlString =`<table class="table" id="dvlist" style="width:100%"> <thead class="thead-light">
+    const nft2Options = {
+        cutoutPercentage: 0,
+        legend: { position: 'left', padding: 5, labels: { pointStyle: 'circle', usePointStyle: true } },
+        plugins: {
+            datalabels: {
+                formatter: (value, ctx) => {
+                    let sum = 0;
+                    let dataArr = ctx.chart.data.datasets[0].data;
+                    dataArr.map(data => {
+                        sum += data;
+                    });
+                    let percentage = (value * 100 / sum).toFixed(2) + "%";
+                    return percentage;
+                },
+                color: '#fff',
+            }
+        },
+        title: {
+            display: true,
+            text: 'Issued NFTs Distribution Since Today'
+        }
+
+
+    };
+    let htmlString = ` <div class="table-responsive"><table class="table" id="dvlist" style="width:100%"> <thead class="thead-light">
     <tr>
-    <th >Image</th>
+    <th ></th>
       <th >Name</th>
-      <th >Count</th>
-      <th >Percentage</th>
-      <th >Total DEGO Amount</th>
+      <th >Today Count</th>
+      <th >Total Count</th>
+      <th >Percent</th>
     </tr></thead><tbody>`;
-    let stats = await getNftStats();
+    let start = new Date();
+    start.setHours(0, 0, 0, 0);
+    let stats = await getNftStats(0);
+    let stats2 = await getNftStats(start.getTime() / 1000);
     let labels = [];
     let data = [];
-    let totalCount=0;
-    let totalDegoAmount=0;
-    let totalValue=0;
+    let labels2 = [];
+    let data2 = [];
+    let totalCount = 0;
+    let totalValue = 0;
+    let totalDegoAmount = 0;
+    let totalTodayCount =0;
     for (let d of stats) {
         
         htmlString += `<td><img src="${d.image}" width="50" height="50"></span></td>`;
         htmlString += `<td><span>${d.name}</span></td>`;
+        for(let s of stats2){
+            if(s.name===d.name){
+                htmlString += `<td><span>${s.count}</span></td>`;
+            }
+            if(d.name==='KRYPTONITE'){
+                htmlString += `<td><span>0</span></td>`;
+            }
+        }
         htmlString += `<td><span>${d.count}</span></td>`;
         htmlString += `<td><span>${d.percentage}%</span></td>`;
-        htmlString += `<td><span>${d.value}</span></td>`;
         htmlString += '</tr>';
         labels.push(d.name);
         data.push(d.count);
-        totalCount +=d.count;
-        totalDegoAmount +=d.degoAmount;
-        totalValue +=d.value;
+        totalCount += d.count;
+        totalDegoAmount += d.degoAmount;
+        totalValue += d.value;
+    }
+    for (let d of stats2) {
+        labels2.push(d.name);
+        data2.push(d.count);
+        totalTodayCount +=d.count;
     }
     htmlString += '</tr>';
-    htmlString += `</tbody></table>`;
+    htmlString += `</tbody></table></div>`;
 
     let nftData = {
         labels: labels,
@@ -110,8 +158,18 @@ $(document).ready(async function () {
             }
         ]
     };
+    let nft2Data = {
+        labels: labels2,
+        datasets: [
+            {
+                backgroundColor: colors.slice(0, 6),
+                borderWidth: 0,
+                data: data2
+            }
+        ]
+    };
 
-    var nft = document.getElementById("nft");
+    var nft = document.getElementById("total");
     if (nft) {
         new Chart(nft, {
             type: 'pie',
@@ -119,16 +177,35 @@ $(document).ready(async function () {
             options: nftOptions
         });
     }
+    var nft2 = document.getElementById("today");
+    if (nft) {
+        new Chart(nft2, {
+            type: 'pie',
+            data: nft2Data,
+            options: nft2Options
+        });
+    }
     $('div#stats').html(htmlString);
     let summary = `<section id="our-stats">
   <div class="row text-center">
+          
+     <div class="col">
+          
+              <div class="counter">
+              <i class="fab fa-ethereum  fa-2x"></i>
+
+                  <h2 class="timer count-title count-number" data-to="100" data-speed="1500">${totalTodayCount}</h2>
+                  <p class="count-text ">Total Number of NFTs Issued TODAY</p>
+              </div>
+          
+      </div>
       <div class="col">
           
               <div class="counter">
               <i class="fab fa-ethereum  fa-2x"></i>
 
                   <h2 class="timer count-title count-number" data-to="100" data-speed="1500">${totalCount}</h2>
-                  <p class="count-text ">Total Number of NFTs</p>
+                  <p class="count-text ">Total Number of NFTs Issued</p>
               </div>
           
       </div>
@@ -141,7 +218,9 @@ $(document).ready(async function () {
               </div>
           
       </div>
-    
+      
+  
+
   
   </div>
 </section>`;
